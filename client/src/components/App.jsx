4df7a2cfea49dce234946491cbf6d1f65092c538
Andrew Heim <andrewsheim@gmail.com>
2022-12-01 14:01:51 -0800

    Added Airbnb esLint, fixed linting on App.js

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,41 +1,40 @@
-import React, { useEffect } from "react";
-import QA from "./QA.jsx"
-import Overview from './productdetails/Overview.jsx';
+import React, { useEffect } from 'react';
 import axios from 'axios';
+import QA from './QA.jsx';
+import Overview from './productdetails/Overview.jsx';
 
 export default function App() {
-
   const getProducts = () => {
     axios.get('/api/products/', {
 
     })
-    .then(response => {
-      console.log(response.data);
-    }).catch(error => {
-      console.log(error);
-    })
-  }
+      .then((response) => {
+        console.log(response.data);
+      }).catch((error) => {
+        console.log(error);
+      });
+  };
 
   const getProduct = () => {
     axios.get('/api/products/40344', {
 
     })
-    .then(response => {
-      console.log(response.data);
-    }).catch(error => {
-      console.log(error);
-    })
-  }
+      .then((response) => {
+        console.log(response.data);
+      }).catch((error) => {
+        console.log(error);
+      });
+  };
 
   const getReviews = () => {
     axios.get('/api/reviews?product_id=40344', {
 
     })
-    .then(response => {
-      console.log(response.data);
-    }).catch(error => {
-      console.log(error);
-    })
+      .then((response) => {
+        console.log(response.data);
+      }).catch((error) => {
+        console.log(error);
+      });
   };
 
   const postReview = () => {
@@ -48,23 +47,23 @@ export default function App() {
       name: 'james',
       email: 'bigballerjames@gmail.com',
       photos: [],
-      characteristics: {}
+      characteristics: {},
     })
-    .then(response => {
-      console.log(response.data);
-    }).catch(error => {
-      console.log(error);
-    })
+      .then((response) => {
+        console.log(response.data);
+      }).catch((error) => {
+        console.log(error);
+      });
   };
 
   return (
     <div>
       <Overview />
       <QA />
-      <button onClick={postReview}>Post Review</button>
-      <button onClick={getReviews}>Get Reviews</button>
-      <button onClick={getProducts}>Get Products</button>
-      <button onClick={getProduct}>Get One Product</button>
+      <button type="button" onClick={postReview}>Post Review</button>
+      <button type="button" onClick={getReviews}>Get Reviews</button>
+      <button type="button" onClick={getProducts}>Get Products</button>
+      <button type="button" onClick={getProduct}>Get One Product</button>
     </div>
-  )
+  );
 }
