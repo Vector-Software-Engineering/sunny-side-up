766936716fac980c9aae196cd71bec41f62c16e1
James Leakos <jamesleakos@gmail.com>
2022-12-01 17:40:17 -0700

    merging in changes

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
-import axios from 'axios';
 
 import QA from './QA.jsx';
 import Overview from './productdetails/Overview.jsx';
