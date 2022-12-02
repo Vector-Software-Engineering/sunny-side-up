dfc65547189ec3e47eae496ff613454b228b2052
Andrew Heim <andrewsheim@gmail.com>
2022-12-02 12:25:35 -0800

    some progress on AddToCard, going to fix changes before pull request, going to pull down Jakes changes on Ratings and Reviews

--- client/src/components/productdetails/AddToCart.jsx
+++ client/src/components/productdetails/AddToCart.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
-//import Select from 'react-select';
+import Dropdown from 'react-dropdown';
 
 const AddToCart = ({ currentStyle }) => {
 
-  //console.log('current style inside of addToCart', currentStyle.skus);
+  console.log('current style inside of addToCart', currentStyle);
+  let sizes = [];
+  if (currentStyle.skus !== undefined) {
+    for (let keys in currentStyle.skus) {
+      console.log(keys, currentStyle.skus[keys].size);
+      sizes.push(currentStyle.skus[keys].size);
+    }
+  }
 
-  // for (let keys of currentStyle.skus) {
-  //   console.log(keys, currentStyle.skus[keys]);
-  // }
+  console.log('sizes are: ', sizes);
 
+  const options = [
+    'one', 'two', 'three'
+  ];
+  // const defaultOption = options[0];
+  // <Dropdown options={options} onChange={this._onSelect} value={defaultOption} placeholder="Select an option" />;
+  //<Dropdown sizes={sizes} value={sizes[0]} placeholder='select an option'/>
   return (
     <div>
       AddToCart Component
+      <Dropdown options={sizes} value='Select Size' placeholder='select an option'/>
     </div>
   );
 };
