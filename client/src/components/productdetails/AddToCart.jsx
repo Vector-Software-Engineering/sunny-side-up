aa4d4902c1688808137d9a5b1049a38e02081a6c
Andrew Heim <andrewsheim@gmail.com>
2022-12-02 16:54:21 -0800

    Added react-select dependency + converting over

--- client/src/components/productdetails/AddToCart.jsx
+++ client/src/components/productdetails/AddToCart.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Dropdown from 'react-dropdown';
+import Select from 'react-select';
 
 const AddToCart = ({ currentStyle }) => {
 
@@ -10,32 +10,35 @@ const AddToCart = ({ currentStyle }) => {
   let quantity = [];
   if (currentStyle.skus !== undefined) {
     for (let keys in currentStyle.skus) {
-      console.log(keys, currentStyle.skus[keys].size);
       if (currentStyle.skus[keys].quantity > 0) {
         sizes.push({label: currentStyle.skus[keys].size, value: keys, quantity: currentStyle.skus[keys].quantity});
-        quantity.push({label: currentStyle.skus[keys].quantity, value: keys})
       }
     }
   }
 
-  // console.log('sizes are: ', sizes);
-  // console.log('sizes are: ', quantity);
+  //console.log('sizes are: ', sizes);
+  //console.log('quantites are: ', quantity);
 
   const grabStyleID = (styleId) => {
-    console.log('grabStyleId on change', styleId);
+    //console.log('grabStyleId on change', styleId);
     let totalQuantity = currentStyle.skus[styleId].quantity;
     let newQuantityOptions = [...Array(Math.min(15, totalQuantity)).keys()];
-    //console.log('newQuantityOptions is: ', newQuantityOptions);
-    setQuantityOptions(newQuantityOptions);
+    let finalQuantityOptions = newQuantityOptions.map(x => x + 1);
+    //console.log('newQuantityOptions is: ', finalQuantityOptions);
+    setQuantityOptions(finalQuantityOptions);
+    //console.log('quantity options after setting new vals: ', quantityOptions)
   };
 
   return (
     <div>
       AddToCart Component
-      {sizes.length > 0 ? <Dropdown options={sizes} onChange={(e) => grabStyleID(e.value)} value='Select Size'/> : <div>OUT OF STOCK</div>}
-      {sizes.length > 0 ? <Dropdown options={quantityOptions} value='-' /> : <Dropdown disabled options={quantity} value='-'/>}
+      {sizes.length > 0 ? <Select options={sizes} onChange={(e) => grabStyleID(e.value)} placeholder={'Select Size'}/> : <div>OUT OF STOCK</div>}
+      {sizes.length > 0 ? <Select options={sizes} placeholder='-' /> : <Select disabled options={quantityOptions} value='-'/>}
     </div>
   );
 };
+
+//{sizes.length > 0 ? <Dropdown options={sizes} onChange={(e) => grabStyleID(e.value)} value='Select Size'/> : <div>OUT OF STOCK</div>}
+//{sizes.length > 0 ? <Dropdown options={quantityOptions} value='-' /> : <Dropdown disabled options={quantity} value='-'/>}
 
 export default AddToCart;
