6629f871d0a1785e3334e1f51ddd5c4b507899c0
Andrew Heim <andrewsheim@gmail.com>
2022-12-02 16:10:17 -0800

    testing select drop down menu with npm react-dropdown. Going to try npm react-select

--- client/src/components/productdetails/AddToCart.jsx
+++ client/src/components/productdetails/AddToCart.jsx
@@ -1,29 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Dropdown from 'react-dropdown';
 
 const AddToCart = ({ currentStyle }) => {
 
-  console.log('current style inside of addToCart', currentStyle);
+  const [quantityOptions, setQuantityOptions] = useState([]);
+
+  //console.log('current style inside of addToCart', currentStyle);
   let sizes = [];
+  let quantity = [];
   if (currentStyle.skus !== undefined) {
     for (let keys in currentStyle.skus) {
       console.log(keys, currentStyle.skus[keys].size);
-      sizes.push(currentStyle.skus[keys].size);
+      if (currentStyle.skus[keys].quantity > 0) {
+        sizes.push({label: currentStyle.skus[keys].size, value: keys, quantity: currentStyle.skus[keys].quantity});
+        quantity.push({label: currentStyle.skus[keys].quantity, value: keys})
+      }
     }
   }
 
-  console.log('sizes are: ', sizes);
+  // console.log('sizes are: ', sizes);
+  // console.log('sizes are: ', quantity);
 
-  const options = [
-    'one', 'two', 'three'
-  ];
-  // const defaultOption = options[0];
-  // <Dropdown options={options} onChange={this._onSelect} value={defaultOption} placeholder="Select an option" />;
-  //<Dropdown sizes={sizes} value={sizes[0]} placeholder='select an option'/>
+  const grabStyleID = (styleId) => {
+    console.log('grabStyleId on change', styleId);
+    let totalQuantity = currentStyle.skus[styleId].quantity;
+    let newQuantityOptions = [...Array(Math.min(15, totalQuantity)).keys()];
+    //console.log('newQuantityOptions is: ', newQuantityOptions);
+    setQuantityOptions(newQuantityOptions);
+  };
+
   return (
     <div>
       AddToCart Component
-      <Dropdown options={sizes} value='Select Size' placeholder='select an option'/>
+      {sizes.length > 0 ? <Dropdown options={sizes} onChange={(e) => grabStyleID(e.value)} value='Select Size'/> : <div>OUT OF STOCK</div>}
+      {sizes.length > 0 ? <Dropdown options={quantityOptions} value='-' /> : <Dropdown disabled options={quantity} value='-'/>}
     </div>
   );
 };
