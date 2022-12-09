db08d694bdd2faab4ca8409d2910b4aff727db74
Andrew Heim <andrewsheim@gmail.com>
2022-12-09 11:16:12 -0800

    Image Gallery - Only Loads 7 thumbnails + loads the other thumbnails when you click on the right arrow - Loads back to 7, small bug where if you click instantly it takes a second click to load - Also need to make the setShortened thumbanils pass down so it triggers on style change

--- client/src/components/productdetails/ImageGallery.jsx
+++ client/src/components/productdetails/ImageGallery.jsx
@@ -1,18 +1,21 @@
 /* eslint-disable no-nested-ternary */
 import React, { useState, useEffect } from 'react';
 import Image from './Image.jsx';
-import { StyledImageGallery, StyledThumbnails, StyleLeftButton, StyleRightButton, StyledMainImage } from './styles/ImageGallery.styled.js';
+import {
+  StyledImageGallery, StyledThumbnails, StyleLeftButton, StyleRightButton, StyledMainImage
+} from './styles/ImageGallery.styled.js';
 
 function ImageGallery({
   currentProduct, currentStyle, mainImage, setMainImage,
   setCurrentIndex, currentIndex, goToExtendedView, firstIndex, setFirstIndex,
 }) {
-  const [shortenedThumbnails, setShortenedThumbnails] = useState([]);
+  const [shortenedThumbnails, setShortenedThumbnails] = useState(undefined);
 
   const setThumbnails = () => {
     console.log('the firstIndex is:, ', firstIndex);
-    if (currentStyle && shortenedThumbnails.length === 0) {
-      let tempArray = [];
+    // eslint-disable-next-line max-len
+    if (currentStyle && (shortenedThumbnails !== undefined || firstIndex === 7 || firstIndex === 0)) {
+      const tempArray = [];
       let counter = 0;
       for (let i = firstIndex; i < currentStyle.photos.length; i += 1) {
         if (counter > 6) {
@@ -25,18 +28,22 @@ function ImageGallery({
       setShortenedThumbnails(tempArray);
     }
   };
-  setThumbnails();
 
   const leftButton = () => {
     if (currentIndex !== 0) {
       setCurrentIndex(currentIndex - 1);
+      if (currentIndex < 9) {
+        setFirstIndex(0);
+        setThumbnails();
+      }
+      console.log('inside leftButton the currentIndex is: ', currentIndex, firstIndex, shortenedThumbnails);
     }
   };
 
   const rightButton = () => {
     if (currentIndex !== currentStyle.photos.length - 1) {
       setCurrentIndex(currentIndex + 1);
-      if (currentIndex > 6) {
+      if (currentIndex > 4) {
         setFirstIndex(7);
         setThumbnails();
       }
@@ -48,25 +55,36 @@ function ImageGallery({
     <StyledImageGallery>
 
       <StyledThumbnails>
-      {!currentStyle
+        {!currentStyle
+          ? <StyleLeftButton onClick={leftButton}> </StyleLeftButton>
+          : currentIndex === 0
             ? <StyleLeftButton onClick={leftButton}> </StyleLeftButton>
-            : currentIndex === 0
-              ? <StyleLeftButton onClick={leftButton}> </StyleLeftButton>
-              : <StyleLeftButton onClick={leftButton}>←</StyleLeftButton>}
-        {shortenedThumbnails.length !== 0
-          ? currentStyle.photos.map((photo, index) => <Image key={index} index={index} photo={photo} setMainImage={setMainImage} setCurrentIndex={setCurrentIndex} currentStyle={currentStyle} />)
-          : null}
-          {!currentStyle
-            ? <StyleRightButton onClick={rightButton}>→</StyleRightButton>
-            : currentIndex === currentStyle.photos.length - 1
-              ? <StyleRightButton onClick={rightButton}>→</StyleRightButton>
-              : <StyleRightButton onClick={rightButton}>→</StyleRightButton>}
+            : <StyleLeftButton onClick={leftButton}>←</StyleLeftButton>}
+        {shortenedThumbnails
+          // eslint-disable-next-line max-len
+          ? shortenedThumbnails.map((photo, index) => <Image key={index} index={index} photo={photo} setMainImage={setMainImage} setCurrentIndex={setCurrentIndex} currentStyle={currentStyle} />)
+          : currentStyle
+            // eslint-disable-next-line max-len
+            ? currentStyle.photos.map((photo, index) => {
+              if (index < 7) {
+                // eslint-disable-next-line max-len
+                return <Image key={index} index={index} photo={photo} setMainImage={setMainImage} setCurrentIndex={setCurrentIndex} currentStyle={currentStyle} />;
+              }
+            })
+            : null}
+        {!currentStyle
+          ? <StyleRightButton onClick={rightButton}> </StyleRightButton>
+          : currentIndex === currentStyle.photos.length - 1
+            ? <StyleRightButton onClick={rightButton}> </StyleRightButton>
+            : <StyleRightButton onClick={rightButton}>→</StyleRightButton>}
       </StyledThumbnails>
 
       {!currentStyle
         ? null
         : mainImage === ''
+          // eslint-disable-next-line max-len
           ? <StyledMainImage onClick={goToExtendedView} src={currentStyle.photos[0].thumbnail_url} />
+          // eslint-disable-next-line max-len
           : <StyledMainImage onClick={goToExtendedView} src={mainImage.photos[currentIndex].thumbnail_url} />}
     </StyledImageGallery>
   );
