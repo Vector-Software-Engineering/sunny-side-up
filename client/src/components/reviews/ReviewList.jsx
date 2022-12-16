4e9dd1bc7ea7a08c6b0c51b831b773a5951aae52
Jake Alexander <jaketalexander@gmail.com>
2022-12-16 10:29:32 -0800

    Lazy load Review Modal

--- client/src/components/reviews/ReviewList.jsx
+++ client/src/components/reviews/ReviewList.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react"
+import React, { useState, Suspense } from "react"
 
 import ReviewTile from "./ReviewTile.jsx";
-import AddReviewModal from "./AddReviewModal.jsx";
 import { ReviewList } from './styles/ReviewList.styled.js';
+
+const AddReviewModal = React.lazy(() => import("./AddReviewModal.jsx"));
 
 export default function ({ product, reviews, visReviews, numReviews, sortBy, setSortBy, setNumReviews, getReviews, meta }) {
 
@@ -15,7 +16,7 @@ export default function ({ product, reviews, visReviews, numReviews, sortBy, set
       <div className='helper'>
         <span>Sort reviews by - </span>
         <span className='pointer' style={ sortBy==='helpful' ? {'fontWeight' : 'bold'} : null } onClick={ () => setSortBy('helpful') }> helpful </span>
-        <span> - </span> 
+        <span> - </span>
         <span className='pointer'  style={ sortBy==='newest' ? {'fontWeight' : 'bold'} : null } onClick={ () => setSortBy('newest') }>newest </span>
         <span> - </span>
         <span className='pointer'  style={ sortBy==='relevance' ? {'fontWeight' : 'bold'} : null } onClick={ () => setSortBy('relevance') }>relevance</span>
@@ -27,13 +28,18 @@ export default function ({ product, reviews, visReviews, numReviews, sortBy, set
       }
       <div className='helper pointer'>
         {
-          numReviews < reviews.length ? 
-          <div onClick={() => { setNumReviews(numReviews + 2); }}>Load more reviews</div> : 
+          numReviews < reviews.length ?
+          <div onClick={() => { setNumReviews(numReviews + 2); }}>Load more reviews</div> :
           <div onClick={() => { setNumReviews(2); }}>Show fewer reviews</div>
         }
       </div>
 
-      {(showModal && meta.characteristics) && <AddReviewModal product={product} setShowModal={setShowModal} characteristics={meta.characteristics} />}
+      {(showModal && meta.characteristics)
+      && (
+      <Suspense fallback={<div>Loading</div>}>
+        (<AddReviewModal product={product} setShowModal={setShowModal} characteristics={meta.characteristics} />)
+      </Suspense>
+      )}
     </ReviewList>
-  )
+  );
 }
